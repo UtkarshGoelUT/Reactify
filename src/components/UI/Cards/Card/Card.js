4ceb02cca75d8f1b7c67aca4f2412fbbc7302cb0
Arsh Kashyap <arshk102001@gmail.com>
2020-11-13 12:19:07 +0530

    custom links to each ngo

--- src/components/UI/Cards/Card/Card.js
+++ src/components/UI/Cards/Card/Card.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React,{Component} from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import classes from "./Card.module.css";
+import {Link} from 'react-router-dom'
 
-const card = (props) => {
+class Cards extends Component{
+    render()
+    {
+        let str="Ngo/"+this.props.name;
 	return (
 		<div>
 			<Card className={classes.Card}>
-				<Card.Header as="h5">{props.name}</Card.Header>
+				<Card.Header as="h5">{this.props.name}</Card.Header>
 				<Card.Body>
-					<h6>Fund Raised: ₹ {props.fund}</h6>
-					<Card.Text>{props.description}</Card.Text>
-					<Button size="sm" variant="info">
-						Go somewhere
-					</Button>
+					<h6>Fund Raised: ₹ {this.props.fund}</h6>
+					<Card.Text>{this.props.description}</Card.Text>
+                    
+					<Link to={str}>
+                        <Button size="sm" variant="info">
+                            Go somewhere
+                        </Button>
+                    </Link>
 				</Card.Body>
 			</Card>
 		</div>
-	);
-};
+    );
+    }
+} 
 
-export default card;
+export default Cards;
